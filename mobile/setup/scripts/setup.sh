3e25b62fb9786e7f28469db75292a2d28e9a60e2
Don Corley <dcorley@usc.edu>
2017-03-21 10:26:31 -0700

    Add auto-deploy scripts

--- mobile/setup/scripts/setup.sh
+++ mobile/setup/scripts/setup.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 if [ ! -d "/var/www/html/php.ini" ]; then
     mv /scripts/php.ini /var/www/html/
-    wp core install --url=http://mobile.usc.edu --title=USC_Gateway_for_Mobile --admin_user=wp-admin --admin_email=mobile@usc.edu --admin_password=${USC_MOBILE_ADMIN_PASSWORD}
+    wp core install --url=https://mobile.usc.edu --title=USC-Gateway-for-Mobile --admin_user=wp-admin --admin_email=mobile@usc.edu --admin_password=${USC_MOBILE_ADMIN_PASSWORD}
     wp theme install --activate /workfiles/mobile-gateway.zip
-    rm -f /workfiles/mobile-gateway.zip
     wp plugin install --activate /workfiles/usc-localist-for-wordpress.zip
-    rm -f /workfiles/usc-localist-for-wordpress.zip
     wp plugin install wordpress-importer --activate
     wp import --authors=create /workfiles/uscgatewayformobile.wordpress.xml
-    rm -f /workfiles/uscgatewayformobile.wordpress.xml
+    wp rewrite structure '/%postname%/'
 fi
