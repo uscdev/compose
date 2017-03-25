4313c066f0900cd5c3cb8dbbe5f3c1f861b7a2d3
Don Corley <don@donandann.com>
2017-03-24 18:46:00 -0700

    Don't set defaults if set

--- mobile/resetanddeploy.sh
+++ mobile/resetanddeploy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 if [ "$1" = "aws" ] || [ "$1" = "" ] ; then
-    export USC_MOBILE_PORT=80
+    if [ "$USC_MOBILE_PORT" = "" ]; then
+        export USC_MOBILE_PORT=80
+    fi
     if [ "$USC_MOBILE_DB_PASSWORD" = "" ]; then
         echo "Enter the database password:"
         read DB_PASSWORD
         export USC_MOBILE_DB_PASSWORD=$DB_PASSWORD
     fi;
-    export ServerName=mobile.usc.edu
+    if [ "$ServerName" = "" ]; then
+        export ServerName=mobile.usc.edu
+    fi
     if [ "$USC_MOBILE_ADMIN_PASSWORD" = "" ]; then
         echo "Enter the Wordpress admin password:"
         read ADMIN_PASSWORD
