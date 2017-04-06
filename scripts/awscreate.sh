721e5e89022289d502229f2cb0dad717282a89ce
Don Corley <dcorley@usc.edu>
2017-04-06 14:02:21 -0700

    Add tags to image

--- scripts/awscreate.sh
+++ scripts/awscreate.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-aws ec2 run-instances --region us-west-1 --image-id ami-a1bae1c1 --block-device-mappings file://mappings.json --count 1 --instance-type m4.large --key-name $USC_AWS_US_WEST_1_KEY_NAME --security-groups Web\ Server
+aws ec2 run-instances --region us-west-1 --image-id ami-a1bae1c1\
+ --block-device-mappings file://mappings.json --count 1 --instance-type m4.large\
+ --key-name $USC_AWS_US_WEST_1_KEY_NAME --security-groups Web\ Server\
+ --tag-specifications ResourceType=string,Tags=[{Key=Name,Value=mobile-usc-edu},{Key=Owner,Value=bsa},{Key=Project,Value=mobile},{Key=Contact,Value=Don\ Corley},{Key=Email,Value=dcorley@usc.edu}]
