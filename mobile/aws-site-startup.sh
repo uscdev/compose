d307d7d81f8dfe447c93e25bf07b9e746bab471c
charanb1r <charanbir7@gmail.com>
2017-04-19 09:58:53 -0700

    Automatic creation of new instance from AMI

--- mobile/aws-site-startup.sh
+++ mobile/aws-site-startup.sh
@@ -1,9 +1,9 @@
-export IMAGE_ID=ami-a1bae1c1
 export INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=mobile.usc.edu" --query Reservations[0].Instances[0].InstanceId --output text)
 
 if [ "$INSTANCE_ID" = "None" ]; then
-	aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type m4.large --key-name its-bsa-prod-us-west-1-key-pair --security-group-ids sg-733f4b14 --subnet-id subnet-5207a536 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test.usc.edu}]' 
-	export INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=test.usc.edu" --query Reservations[0].Instances[0].InstanceId --output text)
+	export IMAGE_ID=ami-a1bae1c1
+	aws ec2 run-instances --image-id $IMAGE_ID --count 1 --instance-type m4.large --key-name its-bsa-prod-us-west-1-key-pair --security-group-ids sg-733f4b14 --subnet-id subnet-5207a536 --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=mobile.usc.edu}]' 
+	export INSTANCE_ID=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=mobile.usc.edu" --query Reservations[0].Instances[0].InstanceId --output text)
 fi
 
 aws ec2 start-instances --instance-ids $INSTANCE_ID
