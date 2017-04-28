0df5d7ed4fb388ec0e6903df4f7a71a5b06b523c
Don Corley <dcorley@usc.edu>
2017-04-28 16:49:32 -0700

    Add secrets filter

--- scripts/archivesecrets.sh
+++ scripts/archivesecrets.sh
@@ -1,4 +1,9 @@
+export USC_FILTER=$1
+if [ "$USC_FILTER" = "" ]; then
+  export USC_FILTER=secrets
+fi
 cd /run
-rm $HOME/secrets.tar.gz.enc
+mkdir -p $HOME/secrets
+rm $HOME/secrets/$USC_FILTER.tar.gz.enc
 sudo echo
-sudo tar cz secrets | openssl des3 -out $HOME/secrets.tar.gz.enc
+sudo tar cz $(cat ./secrets/scripts/filter/$USC_FILTER) | openssl des3 -out $HOME/secrets/$USC_FILTER.tar.gz.enc
--- scripts/restoresecrets.sh
+++ scripts/restoresecrets.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-
+export USC_FILTER=$1
+if [ "$USC_FILTER" = "" ]; then
+  export USC_FILTER=$USER
+fi
+cd /run
 sudo echo
-sudo openssl des3 -d -in $HOME/secrets.tar.gz.enc | sudo tar -C /run -xz
+sudo openssl des3 -d -in $HOME/secrets/$USC_FILTER.tar.gz.enc | sudo tar -C /run -xz
