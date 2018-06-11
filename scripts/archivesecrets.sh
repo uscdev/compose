31ad17820ca689f56fbcfdfc171fb1912c75542b
Don Corley <dcorley@usc.edu>
2018-06-11 16:15:02 -0400

    change secrets directory

--- scripts/archivesecrets.sh
+++ scripts/archivesecrets.sh
@@ -3,7 +3,7 @@ if [ "$USC_FILTER" = "" ]; then
   export USC_FILTER=secrets
 fi
 cd /run
-mkdir -p $HOME/secrets
-rm $HOME/secrets/$USC_FILTER.tar.gz.enc
+mkdir -p $HOME/workspace/secrets
+rm $HOME/workspace/secrets/$USC_FILTER.tar.gz.enc
 sudo echo
-sudo tar cz $(cat ./secrets/scripts/filter/$USC_FILTER) | openssl des3 -out $HOME/secrets/$USC_FILTER.tar.gz.enc
+sudo tar cz $(cat ./secrets/scripts/filter/$USC_FILTER) | openssl des3 -out $HOME/workspace/secrets/$USC_FILTER.tar.gz.enc
