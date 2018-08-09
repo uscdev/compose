0443caa48b2d4df51b9044f21447c8e98ab6a022
Don Corley <dcorley@usc.edu>
2018-08-08 21:01:45 -0700

    Set disk in compose files

--- scripts/restoresecrets.sh
+++ scripts/restoresecrets.sh
@@ -3,6 +3,9 @@ export USC_FILTER=$1
 if [ "$USC_FILTER" = "" ]; then
   export USC_FILTER=$USER
 fi
+if [ "$SECRETS_FILE_PATH" = "" ]; then
+  export SECRETS_FILE_PATH=$HOME/workspace/secrets
+fi
 cd /run
 sudo echo
-sudo openssl des3 -d -in $HOME/workspace/secrets/$USC_FILTER.tar.gz.enc | sudo tar -C /run -xz
+sudo openssl des3 -d -in $SECRETS_FILE_PATH/$USC_FILTER.tar.gz.enc | sudo tar -C /run -xz
