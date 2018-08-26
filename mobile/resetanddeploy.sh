c78d4f6aa9b19b574f9607157de34b720fa123ba
Don Corley <dcorley@usc.edu>
2018-08-26 12:59:32 -0700

    Set up test files

--- mobile/resetanddeploy.sh
+++ mobile/resetanddeploy.sh
@@ -41,6 +41,7 @@ docker volume rm mobile-db
 docker volume rm mobile-data
 
 echo Create new environment
+docker version
 cat docker-compose.yml
 docker stack deploy --compose-file docker-compose.yml mobile
 echo ...Please wait while the environment is created
