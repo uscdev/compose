351abca473315e6c6af8a2fcef843a9a47c8a2ea
Don Corley <dcorley@usc.edu>
2018-09-10 19:25:52 -0700

    Convert to linux format

--- scripts/docker-secret-create-prod.sh
+++ scripts/docker-secret-create-prod.sh
@@ -14,7 +14,16 @@ docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
 docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
-docker secret create $DOMAIN_NAME-tt_secrets.txt $SECRET_PATH/passwords/usc/workday/tt_secrets.txt
+docker secret create $DOMAIN_NAME-my-groups-api.txt $SECRET_PATH/passwords/usc/iam/emailchoice/my-groups-api.txt
+
+export SITE=itapps
+export DOMAIN_NAME=$SITE.usc.edu
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/csr/$DOMAIN_NAME.key
+docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
+docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
+docker secret create dropbox.usc.edu-my-groups-api.txt $SECRET_PATH/passwords/usc/iam/dropbox/my-groups-api.txt
 
 export SITE=workday-account-translator
 export DOMAIN_NAME=$SITE.usc.edu
@@ -23,7 +32,7 @@ docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
 docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
-docker secret create $DOMAIN_NAME-my-groups-api-dropbox.txt $SECRET_PATH//passwords/usc/iam/dropbox/my-groups-api.txt
+docker secret create $DOMAIN_NAME-tt_secrets.txt $SECRET_PATH/passwords/usc/workday/tt_secrets
 
 export SITE=itapps
 export DOMAIN_NAME=$SITE.usc.edu
--- scripts/restoresecrets.sh
+++ scripts/restoresecrets.sh
@@ -9,4 +9,3 @@ fi
 cd /run
 sudo echo
 sudo openssl des3 -d -in $SECRETS_FILE_PATH/$USC_FILTER.tar.gz.enc | sudo tar -C /run -xz
-
