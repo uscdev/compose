c8de7be66bb47d5a66c524242f3a6560b3f0a068
Don Corley <dcorley@usc.edu>
2018-09-10 20:32:29 -0700

    Add to docker environment setup script

---
+++ scripts/docker-secret-create-cloud.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+export SECRET_PATH=/run/secrets
+export PEM_PATH=$SECRET_PATH/certificates/usc
+
+export SITE=cloud
+export DOMAIN_NAME=$SITE.usc.edu
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+
+export SITE=emailchoice
+export DOMAIN_NAME=$SITE.usc.edu
+docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
+docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
+docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
+docker secret create $DOMAIN_NAME-my-groups-api.txt $SECRET_PATH/passwords/usc/iam/emailchoice/my-groups-api.txt
+
+export SITE=itapps
+export DOMAIN_NAME=$SITE.usc.edu
+docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/csr/$DOMAIN_NAME.key
+docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
+docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
+docker secret create dropbox.usc.edu-my-groups-api.txt $SECRET_PATH/passwords/usc/iam/dropbox/my-groups-api.txt
+
+export SITE=workday-account-translator
+export DOMAIN_NAME=$SITE.usc.edu
+docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
+docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
+docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
+docker secret create $DOMAIN_NAME-tt_secrets.txt $SECRET_PATH/passwords/usc/workday/tt_secrets.txt
+
+export SITE=device-registration
+export DOMAIN_NAME=$SITE.usc.edu
+docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+
+docker network create --driver overlay proxy
--- scripts/docker-secret-create-swarm.sh
+++ scripts/docker-secret-create-swarm.sh
@@ -3,13 +3,12 @@
 export SECRET_PATH=/run/secrets
 export PEM_PATH=$SECRET_PATH/certificates/usc
 
-export SITE=cloud
+export SITE=swarm
 export DOMAIN_NAME=$SITE.usc.edu
 docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 
 export SITE=emailchoice
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
@@ -18,7 +17,6 @@ docker secret create $DOMAIN_NAME-my-groups-api.txt $SECRET_PATH/passwords/usc/i
 
 export SITE=itapps
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/csr/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
@@ -27,19 +25,26 @@ docker secret create dropbox.usc.edu-my-groups-api.txt $SECRET_PATH/passwords/us
 
 export SITE=workday-account-translator
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
 docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
-docker secret create $DOMAIN_NAME-tt_secrets.txt $SECRET_PATH/passwords/usc/workday/tt_secrets
-
-export SITE=itapps
-export DOMAIN_NAME=$SITE.usc.edu
-docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME-tt_secrets.txt $SECRET_PATH/passwords/usc/workday/tt_secrets.txt
 
 export SITE=device-registration
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 
 docker network create --driver overlay proxy
+
+docker volume create -d "cloudstor:aws" \
+  --opt size=10 \
+  --opt backing=relocatable \
+  wd-mongo-data
+#  --opt ebstype=io1 \
+#  --opt iops=1000 \
+
+docker volume create -d "cloudstor:aws" --opt backing=shared jenkins-data
+
+docker volume create -d "cloudstor:aws" --opt backing=shared nexus-data
+
