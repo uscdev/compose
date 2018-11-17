f81a6eb429f2e57df1a7cf5558e853d52ff6da32
Don Corley <dcorley@usc.edu>
2018-11-17 07:24:31 -0800

    Add workday to cloud script

--- scripts/setup/docker-secret-create-cloud.sh
+++ scripts/setup/docker-secret-create-cloud.sh
@@ -5,35 +5,38 @@ export PEM_PATH=$SECRET_PATH/certificates/usc
 
 export SITE=cloud
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 
 export SITE=emailchoice
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
 docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
-docker secret create $DOMAIN_NAME-my-groups-api.txt $SECRET_PATH/passwords/usc/iam/emailchoice/my-groups-api.txt
+docker secret create $DOMAIN_NAME-my-groups-api.txt $SECRET_PATH/passwords/usc/middleware/emailchoice/my-groups-api.txt
 
 export SITE=itapps
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/csr/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
 docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
-docker secret create dropbox.usc.edu-my-groups-api.txt $SECRET_PATH/passwords/usc/iam/dropbox/my-groups-api.txt
+docker secret create dropboxchoice.usc.edu-my-groups-api.txt $SECRET_PATH/passwords/usc/middleware/dropbox/my-groups-api.txt
 
 export SITE=workday-account-translator
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 docker secret create $DOMAIN_NAME.key $PEM_PATH/$DOMAIN_NAME/$DOMAIN_NAME.key
 docker secret create $DOMAIN_NAME-sp-key.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-key.pem
 docker secret create $DOMAIN_NAME-sp-cert.pem $PEM_PATH/shibboleth/$DOMAIN_NAME/sp-cert.pem
 docker secret create $DOMAIN_NAME-tt_secrets.txt $SECRET_PATH/passwords/usc/workday/tt_secrets.txt
+docker secret create dfp_users_backend $SECRET_PATH/passwords/usc/workday/secrets/dfp_users_backend.txt
 
 export SITE=device-registration
 export DOMAIN_NAME=$SITE.usc.edu
-docker secret create $DOMAIN_NAME-cert.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
+docker secret create cert-$DOMAIN_NAME.pem $PEM_PATH/$DOMAIN_NAME/haproxy/cert-$DOMAIN_NAME.pem
 
 docker network create --driver overlay proxy
 docker network create --driver overlay monitor
+
+docker volume create -d "cloudstor:aws" --opt size=10 --opt backing=relocatable wd-mongo-data
