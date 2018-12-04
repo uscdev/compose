d86134761a0834b080f0db7081873cf0a4b3850a
Don Corley <dcorley@usc.edu>
2018-12-03 20:17:35 -0800

    Fix script

--- scripts/setup/03-docker-volume-create-aws.sh
+++ scripts/setup/03-docker-volume-create-aws.sh
@@ -60,4 +60,4 @@ docker volume create -d "cloudstor:aws" \
   --opt size=2 \
   --opt backing=relocatable \
   guacamole-db
-  docker stack deploy --compose-file ./shared/docker-compose-volume-fix-guacamole.yml guacamole-fix-volume
+docker stack deploy --compose-file ./shared/docker-compose-volume-fix-guacamole.yml guacamole-fix-volume
